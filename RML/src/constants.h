e36335989c0338da29fb9e033c4b483c14c20010
Aaron Meaney <aaronmeaney@hotmail.com>
2022-07-29 13:59:48 +0100

    Begin moving Math code from CppRayTracerChallenge into this static lib. Add Tuple and Vector

---
+++ RML/src/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <numeric>
+
+namespace RML
+{
+	constexpr float EPSILON = 0.00001f;
+
+	/// <summary>
+	/// Positive Infinity
+	/// </summary>
+	constexpr double INF = std::numeric_limits<double>::infinity();
+}
